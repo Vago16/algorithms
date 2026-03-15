0bbb06567104b2c8d7cd7100eb63be6dd94c2980
Vagelis Petropoulos <vagelispetropoulos@yahoo.com>
2026-03-14 21:48:39 -0400

    finished Project1

--- project1/SpellboundTransmutations.py
+++ project1/SpellboundTransmutations.py
@@ -1,5 +1,6 @@
 import sys
-
+sys.setrecursionlimit(10000)  #needed for my implementation of memoized DP approach
+#will take a few more seconds loading the tests after test #8
 ###############################################################################
 # Naive Recursive (Divide-and-Conquer) Approach
 ###############################################################################
@@ -28,7 +29,49 @@ def naive_recursive_edit_distance(source, target, cost_insert, cost_delete, cost
     # cost_trans: cost for swapping adjacent characters
 
     # ... Implementation here ...
-    min_edit_distance = 0  # placeholder
+
+    #base cases
+    if len(source) == 0:
+        return len(target) * cost_insert
+
+    if len(target) == 0:
+        return len(source) * cost_delete
+
+    #if last characters match
+    if source[-1] == target[-1]:
+        return naive_recursive_edit_distance(
+            source[:-1], target[:-1],
+            cost_insert, cost_delete, cost_sub, cost_trans
+        )
+
+    ###Transformation Operations#####
+    #insert operation
+    insert_cost = cost_insert + naive_recursive_edit_distance(
+        source, target[:-1],
+        cost_insert, cost_delete, cost_sub, cost_trans
+    )
+
+    #delete operation
+    delete_cost = cost_delete + naive_recursive_edit_distance(
+        source[:-1], target,
+        cost_insert, cost_delete, cost_sub, cost_trans
+    )
+
+    #substitute operation
+    substitute_cost = cost_sub + naive_recursive_edit_distance(
+        source[:-1], target[:-1],
+        cost_insert, cost_delete, cost_sub, cost_trans
+    )
+
+    min_edit_distance = min(insert_cost, delete_cost, substitute_cost)
+
+    #transposition operation
+    if (len(source) > 1 and len(target) > 1 and source[-1] == target[-2] and source[-2] == target[-1]):
+        transpose_cost = cost_trans + naive_recursive_edit_distance(
+            source[:-2], target[:-2],
+            cost_insert, cost_delete, cost_sub, cost_trans
+        )
+        min_edit_distance = min(min_edit_distance, transpose_cost)
 
     return min_edit_distance
 
@@ -65,8 +108,39 @@ def iterative_edit_distance(source, target, cost_insert, cost_delete, cost_sub, 
 
     # ... Implementation here ...
 
-    dp_cost = 0         # This would be computed from the DP table
-    final_string = ""   # This would be reconstructed from backtracking
+    m = len(source)
+    n = len(target)
+
+    #DP table for dynamic programming
+    dp = [[0]*(n+1) for _ in range(m+1)]
+
+    #base cases
+    for i in range(1, m+1):
+        dp[i][0] = i * cost_delete
+
+    for j in range(1, n+1):
+        dp[0][j] = j * cost_insert
+
+    #fill DP table
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+
+            if source[i-1] == target[j-1]:
+                cost = dp[i-1][j-1]
+            else:
+                cost = dp[i-1][j-1] + cost_sub  #substituion operation
+
+            insert_cost = dp[i][j-1] + cost_insert  #insert operation
+            delete_cost = dp[i-1][j] + cost_delete  #delete operation
+
+            dp[i][j] = min(cost, insert_cost, delete_cost)
+
+            #transposition operation
+            if (i > 1 and j > 1 and source[i-1] == target[j-2] and source[i-2] == target[j-1]):
+                dp[i][j] = min(dp[i][j], dp[i-2][j-2] + cost_trans)
+
+    dp_cost = dp[m][n]
+    final_string = target
 
     return dp_cost, final_string
 
@@ -106,8 +180,86 @@ def memoized_edit_distance(source, target, cost_insert, cost_delete, cost_sub, c
     #       (subsource, subtarget) pairs, improving efficiency
 
     # ... Implementation here ...
+    if memo is None:
+        memo = {}
 
-    min_cost = 0
-    final_string = ""
+    key = (source, target)
 
-    return min_cost, final_string
+    if key in memo:
+        return memo[key]
+
+    #base cases
+    if len(source) == 0:
+        result = (len(target) * cost_insert, target)
+        memo[key] = result
+        return result
+
+    if len(target) == 0:
+        result = (len(source) * cost_delete, "")
+        memo[key] = result
+        return result
+
+    #characters match
+    if source[-1] == target[-1]:
+        cost, string = memoized_edit_distance(
+            source[:-1], target[:-1],
+            cost_insert, cost_delete, cost_sub, cost_trans, memo
+        )
+
+        result = (cost, string + source[-1])
+        memo[key] = result
+        return result
+
+    #insert opetation
+    insert_cost, insert_string = memoized_edit_distance(
+        source, target[:-1],
+        cost_insert, cost_delete, cost_sub, cost_trans, memo
+    )
+    insert_cost += cost_insert
+    insert_string = insert_string + target[-1]
+
+    #delete operation
+    delete_cost, delete_string = memoized_edit_distance(
+        source[:-1], target,
+        cost_insert, cost_delete, cost_sub, cost_trans, memo
+    )
+    delete_cost += cost_delete
+
+    #substitute operation
+    substitute_cost, substitute_string = memoized_edit_distance(
+        source[:-1], target[:-1],
+        cost_insert, cost_delete, cost_sub, cost_trans, memo
+    )
+    substitute_cost += cost_sub
+    substitute_string = substitute_string + target[-1]
+
+    #choose best operation
+    min_cost = insert_cost
+    final_string = insert_string
+
+    if delete_cost < min_cost:
+        min_cost = delete_cost
+        final_string = delete_string
+
+    if substitute_cost < min_cost:
+        min_cost = substitute_cost
+        final_string = substitute_string
+
+    #transposition
+    if (len(source) > 1 and len(target) > 1 and source[-1] == target[-2] and source[-2] == target[-1]):
+        transpose_cost, transpose_string = memoized_edit_distance(
+            source[:-2], target[:-2],
+            cost_insert, cost_delete, cost_sub, cost_trans, memo
+        )
+
+        transpose_cost += cost_trans
+        transpose_string = transpose_string + target[-2] + target[-1]
+
+        if transpose_cost < min_cost:
+            min_cost = transpose_cost
+            final_string = transpose_string
+
+    result = (min_cost, final_string)
+    memo[key] = result
+
+    return result
